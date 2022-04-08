3c2e812ef826e2ac7a0d77bd2384a60d5c362541
Sebastian Arneson <c21sebar@student.his.se>
2022-04-08 11:33:37 +0200

    Fixed layout: 1. Text 2. Button 3. IMG When click on button, show image, click again to hide img

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,13 +3,33 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button shrekButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        shrekButton = findViewById(R.id.ButtonID);
+        shrekButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("KLICK", "onClick: HALLO");
+                if (findViewById(R.id.ImageViewID).getVisibility()== View.VISIBLE){
+                    Log.d("KLICK", "onClick: INVISIBLE");
+                     findViewById(R.id.ImageViewID).setVisibility(View.INVISIBLE);
+                }
+                else if (findViewById(R.id.ImageViewID).getVisibility()== View.INVISIBLE){
+                    Log.d("KLICK", "onClick: VISIBLE");
+                    findViewById(R.id.ImageViewID).setVisibility(View.VISIBLE);
 
+                }
+            }
+        });
     }
+
+
 }
